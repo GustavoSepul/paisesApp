8057649c33aa0db19ab37551ae2bc24b5b52b1a2
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-10 15:42:59 -0300

    componente para buscar por pais
    componente para buscar por pais con su respectivo metodo

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
+  termino: string = '';
+
+  constructor() { }
+
+  buscar() {
+    console.log(this.termino);
+  }
+
 }

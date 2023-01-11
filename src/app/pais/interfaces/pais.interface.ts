3c9a46e8b1fc665553520cb7357d637dd3a01b2e
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-11 17:19:46 -0300

    Fin de la pantalla de ver país
    cambio en la interface

--- src/app/pais/interfaces/pais.interface.ts
+++ src/app/pais/interfaces/pais.interface.ts
@@ -1,106 +1,59 @@
 export interface Country {
-  name:         Name;
-  tld:          string[];
-  alpha2Code:   string;
-  alpha3Code:   string;
-  cca3:         string;
-  cioc?:        string;
-  independent:  boolean;
-  status:       string;
-  unMember:     boolean;
-  currencies:   { [key: string]: Currency };
-  idd:          Idd;
-  capital:      string[];
-  altSpellings: string[];
-  region:       string;
-  subregion:    string;
-  languages:    Languages;
-  translations: { [key: string]: Translation };
-  latlng:       number[];
-  landlocked:   boolean;
-  borders?:     string[];
-  area:         number;
-  demonyms:     Demonyms;
-  flag:         string;
-  maps:         Maps;
-  population:   number;
-  gini?:        { [key: string]: number };
-  fifa?:        string;
-  car:          Car;
-  timezones:    string[];
-  continents:   string[];
-  flags:        CoatOfArms;
-  coatOfArms:   CoatOfArms;
-  startOfWeek:  string;
-  capitalInfo:  CapitalInfo;
-  postalCode?:  PostalCode;
-}
-
-export interface CapitalInfo {
-  latlng?: number[];
-}
-
-export interface Car {
-  signs: string[];
-  side:  string;
-}
-
-export interface CoatOfArms {
-  png?: string;
-  svg?: string;
+  name:           string;
+  topLevelDomain: string[];
+  alpha2Code:     string;
+  alpha3Code:     string;
+  callingCodes:   string[];
+  capital:        string;
+  altSpellings:   string[];
+  region:         string;
+  subregion:      string;
+  population:     number;
+  latlng:         number[];
+  demonym:        string;
+  area:           number;
+  gini:           number;
+  timezones:      string[];
+  borders:        string[];
+  nativeName:     string;
+  numericCode:    string;
+  currencies:     Currency[];
+  languages:      Language[];
+  translations:   Translations;
+  flag:           string;
+  regionalBlocs:  RegionalBloc[];
+  cioc:           string;
 }
 
 export interface Currency {
+  code:   string;
   name:   string;
   symbol: string;
 }
 
-export interface Demonyms {
-  eng:  Eng;
-  fra?: Eng;
+export interface Language {
+  iso639_1:   string;
+  iso639_2:   string;
+  name:       string;
+  nativeName: string;
 }
 
-export interface Eng {
-  f: string;
-  m: string;
+export interface RegionalBloc {
+  acronym:       string;
+  name:          string;
+  otherAcronyms: any[];
+  otherNames:    any[];
 }
 
-export interface Idd {
-  root:     string;
-  suffixes: string[];
-}
-
-export interface Languages {
-  eng?: string;
-  spa?: string;
-  swa?: string;
-  ara?: string;
-}
-
-export interface Maps {
-  googleMaps:     string;
-  openStreetMaps: string;
-}
-
-export interface Name {
-  common:     string;
-  official:   string;
-  nativeName: NativeName;
-}
-
-export interface NativeName {
-  eng?: Translation;
-  spa?: Translation;
-  swa?: Translation;
-  ara?: Translation;
-}
-
-export interface Translation {
-  official: string;
-  common:   string;
-}
-
-export interface PostalCode {
-  format: string;
-  regex:  string;
+export interface Translations {
+  de: string;
+  es: string;
+  fr: string;
+  ja: string;
+  it: string;
+  br: string;
+  pt: string;
+  nl: string;
+  hr: string;
+  fa: string;
 }

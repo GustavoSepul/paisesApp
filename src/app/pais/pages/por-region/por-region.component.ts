54c715936ce2b1371469b4cb1bd8c99b7eb4ef70
GustavoSepul <gsepulvedao@ing.ucsc.cl>
2023-01-12 12:11:25 -0300

    formas de cambiar una clase
    ngClass, class y [class.]

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-por-region',
-  templateUrl: './por-region.component.html'
+  templateUrl: './por-region.component.html',
+  styles: [`
+    button {
+      margin-right: 5px;
+    }
+
+  `
+  ]
 })
 export class PorRegionComponent {
+
+  regiones: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  regionActiva: string ='';
+
+  constructor(){}
+
+  activarRegion( region: string ) {
+    this.regionActiva = region;
+
+    //TODO: hacer el llamado al servicio
+  }
 
 }
